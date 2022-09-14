18385d489af0366a84fc92906b84dce7848877f9
vgowthamvk <vgowthamvk@gmail.com>
2022-09-14 15:13:10 +0530

    added table and snapshot support

--- macros/generate_tasks.sql
+++ macros/generate_tasks.sql
@@ -3,22 +3,43 @@
 {%- set columns = adapter.get_columns_in_relation(source_relation) -%}
 
 {% set column_names = columns | map(attribute='name') %}
-{% set output %}
-{{columns}}
+{% set dtypes = columns | map(attribute='dtype') %}
 
+{% set output %}
+'columns' = {{columns}}
+'source_relation' = {{source_relation}}
+{{source_relation.identifier}}
 {% set stream_name = source_relation.database~'.'~source_relation.schema~'.'~source_relation.identifier~'_STREAM' %}
 CREATE OR REPLACE STREAM {{stream_name}} 
 ON TABLE {{ source(source_name, source_table_name) }};
+
+{% set table_name = source_relation.database~'.'~source_relation.schema~'.RAW_'~source_relation.identifier %}
+CREATE OR REPLACE TABLE {{table_name}} (
+WAREHOUSE VARCHAR(3),
+{%- for column in columns %}
+    {% if  column.is_string() == True %}
+        {{column.column}} {{column.dtype}}({{column.char_size}}),
+    {% elif column.dtype == 'BOOLEAN' %}
+        {{column.column}} {{column.dtype}},
+    {% elif  column.is_number() == True %}
+        {{column.column}} {{column.dtype}}{{column.numeric_precision,column.numeric_scale}},
+    {% endif %}
+{# {% if not loop.last %},{% endif %} #}
+{%- endfor %}
+ MODIFICATION_TIMESTAMP TIMESTAMP_LTZ(9)
+);
+
+INSERT INTO {{table_name}}
+SELECT 'ATL',*,current_timestamp as MODIFICATION_TIMESTAMP FROM {{ source(source_name, source_table_name) }};
 
 {% set task_name = source_name~'_'~source_table_name~'_TASK' %}
 create or replace task {{task_name}}
 warehouse=PRD_ETL_S_CS_WH  schedule ='1 minute' when
 SYSTEM$STREAM_HAS_DATA('{{stream_name}}') as
-INSERT INTO {{dest_relation}}
+INSERT INTO {{table_name}}
 select 'ATL',
 {%- for column_name in column_names %}
-{{column_name}}
-{% if not loop.last %},{% endif %}
+{{column_name}} {% if not loop.last %},{% endif %}
 {%- endfor %}
 , current_timestamp as MODIFICATION_TIMESTAMP
 from {{stream_name}} WHERE METADATA$ACTION='INSERT';
